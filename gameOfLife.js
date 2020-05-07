190252f13b8f0ca188aa37cac5392f2cfd4abade
Alberto Garcia <albertoinformatica97@gmail.com>
2020-05-07 22:00:27 +0200

    Hidden screen setBoard onclick set button

--- gameOfLife.js
+++ gameOfLife.js
@@ -2,9 +2,19 @@ document.onload = (() => {
 
     const slider = document.querySelector('.slider')
     const sliderValueSelected = document.querySelector('.slider-value-selected')
+    const flexSetBoard = document.querySelector('.flex-container-set-board')
+    const btnSetBoard = document.querySelector('#button-set-board')
+
+
+    btnSetBoard.onclick = setBoard
 
     slider.addEventListener('input', () => {
         sliderValueSelected.innerHTML = `Board  <br> ${slider.value} x ${slider.value}`
     })
+
+    
+    function setBoard() {
+        flexSetBoard.style.display = 'none'
+    }
 
 })()
